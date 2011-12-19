a5da14a74169734a5c62165e04324dbf36279d52
mikebaillargeon <md.baillargeon@gmail.com>
2011-12-19 16:48:21 -0500

    switch to forked props

--- products/toro.mk
+++ products/toro.mk
@@ -2,10 +2,10 @@
 $(call inherit-product, device/samsung/toro/full_toro.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/pete/products/common.mk)
+$(call inherit-product, vendor/mike/products/common.mk)
 
 # Setup device specific product configuration.
-PRODUCT_NAME := pete_toro
+PRODUCT_NAME := mike_toro
 PRODUCT_BRAND := google
 PRODUCT_DEVICE := toro
 PRODUCT_MODEL := Galaxy Nexus
@@ -14,27 +14,27 @@ PRODUCT_MANUFACTURER := samsung
 PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=mysid BUILD_ID=ICL53F BUILD_FINGERPRINT=google/mysid/toro:4.0.2/ICL53F/235179:user/release-keys PRIVATE_BUILD_DESC="mysid-user 4.0.2 ICL53F 235179 release-keys" BUILD_NUMBER=235179 BUILD_UTC_DATE=1323307736
 
 # Inherit common build.prop overrides
--include vendor/pete/products/common_versions.mk
+-include vendor/mike/products/common_versions.mk
 
 # Copy toro specific prebuilt files
 PRODUCT_COPY_FILES +=  \
-    vendor/pete/proprietary/tuna/app/Thinkfree.apk:system/app/Thinkfree.apk \
-    vendor/pete/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
-    vendor/pete/proprietary/tuna/media/LMprec_508.emd:system/media/LMprec_508.emd \
-    vendor/pete/proprietary/tuna/media/PFFprec_600.emd:system/media/PFFprec_600.emd \
-    vendor/pete/proprietary/tuna/media/audio/notifications/Nexus.mp3:system/media/audio/notifications/Nexus.mp3 \
-    vendor/pete/proprietary/common/app/Microbes.apk:system/app/Microbes.apk \
-    vendor/pete/proprietary/common/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
-    vendor/pete/proprietary/common/etc/permissions/com.google.android.nfc_extras.xml:system/etc/permissions/com.google.android.nfc_extras.xml \
-    vendor/pete/proprietary/common/framework/com.google.android.media.effects.jar:system/framework/com.google.android.media.effects.jar \
-    vendor/pete/proprietary/common/framework/com.android.nfc_extras.jar:system/framework/com.android.nfc_extras.jar \
-    vendor/pete/proprietary/common/lib/libearthmobile.so:system/lib/libearthmobile.so \
-    vendor/pete/proprietary/common/lib/libmicrobes_jni.so:system/lib/libmicrobes_jni.so
+    vendor/mike/proprietary/tuna/app/Thinkfree.apk:system/app/Thinkfree.apk \
+    vendor/mike/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/mike/proprietary/tuna/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/mike/proprietary/tuna/media/PFFprec_600.emd:system/media/PFFprec_600.emd \
+    vendor/mike/proprietary/tuna/media/audio/notifications/Nexus.mp3:system/media/audio/notifications/Nexus.mp3 \
+    vendor/mike/proprietary/common/app/Microbes.apk:system/app/Microbes.apk \
+    vendor/mike/proprietary/common/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
+    vendor/mike/proprietary/common/etc/permissions/com.google.android.nfc_extras.xml:system/etc/permissions/com.google.android.nfc_extras.xml \
+    vendor/mike/proprietary/common/framework/com.google.android.media.effects.jar:system/framework/com.google.android.media.effects.jar \
+    vendor/mike/proprietary/common/framework/com.android.nfc_extras.jar:system/framework/com.android.nfc_extras.jar \
+    vendor/mike/proprietary/common/lib/libearthmobile.so:system/lib/libearthmobile.so \
+    vendor/mike/proprietary/common/lib/libmicrobes_jni.so:system/lib/libmicrobes_jni.so
 
 # Inherit Face lock security blobs
--include vendor/pete/products/common_facelock.mk
+-include vendor/mike/products/common_facelock.mk
 
 # Inherit drm blobs
--include vendor/pete/products/common_drm.mk
+-include vendor/mike/products/common_drm.mk
 
 
